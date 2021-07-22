18302708258a90b22465b0d840ea8d255096e65e
Rachelle Lee <rachedeveloper@gmail.com>
2021-07-21 20:49:36 -0700

    Initial Commit

---
+++ BetterRest2/BetterRest2App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest2App.swift
+//  BetterRest2
+//
+//  Created by RachelleLee on 7/21/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
